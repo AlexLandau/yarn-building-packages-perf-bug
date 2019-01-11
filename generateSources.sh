eba76bd9781dedce1f8f2ce4b38a8faac05a27ee
Alex Landau <alandau@palantir.com>
2019-01-10 16:35:13 -0800

    Mess around a little more; seems like I've run into another perf issue?

--- generateSources.sh
+++ generateSources.sh
@@ -3,5 +3,13 @@ mkdir packages/
 
 for N in {1..100}; do
   mkdir "packages/package$N"
-  cat shared/package.json | sed "s/replaceMeWithPackageName/package$N/" > "packages/package$N/package.json"
+  INTERNAL_DEPS_STRING=""
+  I=1
+  while [ $I -lt $N ]; do
+    INTERNAL_DEPS_STRING+="    \"package$I\": \"0.0.1\","$'\\\n'
+    let I+=1
+  done
+  cat shared/package.json |
+      sed "s/replaceMeWithPackageName/package$N/" |
+      sed "s/replaceMeWithInternalDependencies/$INTERNAL_DEPS_STRING/" > "packages/package$N/package.json"
 done
